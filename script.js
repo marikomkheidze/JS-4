f927a912b8bc89acedb631baabd3750581cf5e92
marikomkheidze <110020576+marikomkheidze@users.noreply.github.com>
2022-09-30 22:58:48 +0400

    end

---
+++ script.js
@@ -0,0 +1,16 @@
+// დავალება 1
+
+// let array = ['academy', 'of', 'digital', 'industries'].reduce(function (accumulator, currentValue) {
+//     return accumulator.concat(currentValue + " ");
+// }, "")
+// console.log(array);
+
+
+
+// დავალება 2
+
+// let array =[23,45,32,5,87,7,3,98].sort((x, y) => y - x );
+// console.log(array);
+
+// let array2 =[23,45,32,5,87,7,3,98].sort((x, y) => x - y ) [0];
+// console.log(array2);
